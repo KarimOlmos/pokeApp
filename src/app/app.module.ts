b7d7d1f1ea5be0a6e1b889e1ad76676097df2338
Karim Olmos <olmoskarim@gmail.com>
2024-05-07 15:43:42 +0200

    commit tras built

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { SharedModule } from './shared/shared.module'; // Importa SharedModule aquí
+import { SharedModule } from './shared/shared.module';
+import { ServiceWorkerModule } from '@angular/service-worker'; // Importa SharedModule aquí
 
 @NgModule({
   declarations: [
@@ -17,7 +18,13 @@ import { SharedModule } from './shared/shared.module'; // Importa SharedModule a
     HttpClientModule,
     FormsModule,
     PokemonModule,
-    SharedModule // Importa SharedModule aquí
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }) // Importa SharedModule aquí
   ],
   providers: [],
   bootstrap: [AppComponent]
